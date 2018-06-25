b76d5bd90d3d1866f9e5db306619022bbed867d8
rob <hofmanrob@hotmail.com>
2018-06-26 00:53:14 +0200

    limit on get data, not yet "load more" funcntion

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -7,6 +7,7 @@ import { ReactiveVar } from 'meteor/reactive-var'
 
  
 import './body.html';
+// import './body.css';
  
 Template.body.onCreated(function(){
     this.selectedDate = new ReactiveVar("13"); 
@@ -16,7 +17,7 @@ Template.body.helpers({
 
     gfevents() { 
         var re = '/^2018-07-'+Template.instance().selectedDate.get()+'/';
-        return gfevents.find({"startDate":{ $regex: eval(re)}});
+        return gfevents.find({"startDate":{ $regex: eval(re)}},{skip: 0, limit: 5});
     },
 });
 
