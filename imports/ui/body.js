c2c1b1d2abeba42a0a2644b6bc90c599a5e75527
rob <hofmanrob@hotmail.com>
2018-06-26 00:14:51 +0200

    working version with styled date selector

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,27 @@
 import { Template } from 'meteor/templating';
  
 import { gfevents } from '../api/gfevents';
+
+import { ReactiveVar } from 'meteor/reactive-var'
+
+
  
 import './body.html';
  
-Template.body.helpers({
-  gfevents() {
-    return gfevents.find({"startDate":{ $regex: /^2018-07-14/}});
-  },
+Template.body.onCreated(function(){
+    this.selectedDate = new ReactiveVar("13"); 
 });
+
+Template.body.helpers({
+
+    gfevents() { 
+        var re = '/^2018-07-'+Template.instance().selectedDate.get()+'/';
+        return gfevents.find({"startDate":{ $regex: eval(re)}});
+    },
+});
+
+Template.body.events({
+    'change .date-radio': function(event, template){
+        template.selectedDate.set(event.target.value);
+    }
+}); 
